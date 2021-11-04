91cb5ddb53f8a46a58dac179ff3356dfe2d1405d
alvar <alvar@DESKTOP-52OFONO>
2021-11-04 11:59:56 +0000

    Kata3 Version2

--- src/kata3/HistogramDisplay.java
+++ src/kata3/HistogramDisplay.java
@@ -1,6 +1,4 @@
 package kata3;
-import org.jfree.ui.ApplicationFrame;
-import java.awt.Container;
 import java.awt.Dimension;
 import javax.swing.JPanel;
 import org.jfree.chart.ChartFactory;
@@ -8,52 +6,48 @@ import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
-
+import org.jfree.ui.ApplicationFrame;
 
 public class HistogramDisplay extends ApplicationFrame{
+   private final Histogram <String> histogram;
 
-    public HistogramDisplay(String title) {
-        super("HISTOGRAMA");
-        setContentPane(createPanel());
-        pack();
+    public HistogramDisplay(Histogram <String> histogram) {
+        super("Histograma");
+        this.histogram = histogram;
+        this.setContentPane(createPanel());
+        this.pack(); 
     }
-    
-    
-    
+   
     private JPanel createPanel() {
-        ChartPanel chartPanel = new ChartPanel (createChart(createDatase()));
-        setPreferredSize(new Dimension(500,400));
+        ChartPanel chartPanel = new ChartPanel(createChart(createDataset()));
+        chartPanel.setPreferredSize(new Dimension(500, 400));
         return chartPanel;
     }
-
-    
-    
     public void execute(){
         setVisible(true);
     }
-
     
     private JFreeChart createChart(DefaultCategoryDataset dataSet){
-        JFreeChart chart = ChartFactory.createBarChart("Histograma JFreeChart", "Dominios email", "Nº de emails", dataSet, PlotOrientation.VERTICAL, false, false, rootPaneCheckingEnabled);
-                return chart;
+        JFreeChart chart = ChartFactory.createBarChart(
+                "Histograma JFreeChart",    
+                "Dominios",           
+                "Nºde emails",             
+                dataSet,
+                PlotOrientation.VERTICAL,      
+                false,                      
+                false,                      
+                rootPaneCheckingEnabled     
+        );
+        return chart;
     }
-
     
-    private DefaultCategoryDataset createDatase() {
-        
+    private DefaultCategoryDataset createDataset() {
         DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
         
-        dataSet.addValue(7, "","gmail.com");
-        
-        dataSet.addValue(8, "","ulpgc.es");
-        
-        dataSet.addValue(9, "","ull.es");
-        
-        dataSet.addValue(10, "","hotmail.com");
-        
+     for (String key : histogram.keySet()) {
+            dataSet.addValue(histogram.get(key), "", key);
+        }
+
         return dataSet;
     }
-
 }
-    
-
--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -1,17 +1,23 @@
 package kata3;
-
-
 public class Kata3 {
-    
 
+    
     public static void main(String[] args) {
         
+        Histogram <String> histogram = new Histogram();
         
-     HistogramDisplay histo = new HistogramDisplay("JFreeChart");
-     
-     histo.execute();
-     
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("ulpgc.com");
+        histogram.increment("ulpgc.com");
+        histogram.increment("ull.com");
+       
+
+        new HistogramDisplay(histogram).execute();    
     }
 
-    
-} 
+}
+
+  
